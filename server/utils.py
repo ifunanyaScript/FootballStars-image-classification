f6705637a347325f828cc629a609466079c36b5d
Favour Nwajoko <chocorazzee@gmail.com>
2022-08-09 20:13:04 +0100

    update

--- server/utils.py
+++ server/utils.py
@@ -9,8 +9,61 @@ __label_name_number = {}
 __label_number_name = {}
 __model = None
 
+def get_b64_image():
+    with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\server\base64.txt") as f:
+        return f.read()
+
+def base64_to_image(base64_string):
+    encoded_data = base64_string.split(",")[1]
+    nparray = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    image = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
+    return image
+
+def get_clean_image(base64_string, file_path):
+    detect_face = cv2.CascadeClassifier(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\opencv\haarcascade_frontalface_default.xml")
+    detect_eye = cv2.CascadeClassifier(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\opencv\haarcascade_eye.xml")
+
+    if file_path:
+        image = cv2.imread(file_path)
+    else:
+        image = base64_to_image(base64_string)
+    
+    if image is not None:
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face = detect_face.detectMultiScale(gray_image, 1.3, 5)
+
+        cropped_faces = []
+        for (x, y, w, h) in face:
+            region_gray = gray_image[y:y+h, x:x+w]
+            region_colour = image[y:y+h, x:x+w]
+            eyes = detect_eye.detectMultiScale(region_gray)
+            # Check if there are two eyes
+            if len(eyes) >= 2:
+                # Append face if it has two eyes.
+                cropped_faces.append(region_colour)
+    return cropped_faces
+
+def load_aritifacts():
+    print(f"Loading Artifacts...")
+    global __label_name_number
+    global __lable_number_name
+
+    with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\model\artifacts\label_dict.json", "r") as f:
+        __label_name_number = json.load(f)
+        __lable_number_name = {i:k for k,i in __label_name_number.items()}
+        
+    global __model
+    with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\model\artifacts\model.pickle", "rb") as f:
+        __model = pickle.load(f)
+
+    print("Artifacts succesfully loaded!")
+
+def number_name(label_number):
+    return __label_number_name[label_number]
+
 def classify_image(base64_image, file_path=None):
     images = get_clean_image(base64_image, file_path)
+
     prediction = []
 
     for image in images:
@@ -30,61 +83,6 @@ def classify_image(base64_image, file_path=None):
         })
 
     return prediction
-
-def number_name(label_number):
-    return __label_number_name[label_number]
-
-
-def load_aritifacts():
-    print(f"Loading Artifacts...")
-    global __label_name_number
-    global __lable_number_name
-
-    with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\model\artifacts\label_dict.json", "r") as f:
-        __label_name_number = json.load(f)
-        __lable_number_name = {i:k for k,i in __label_name_number.items()}
-        
-    global __model
-    if __model is not None:
-        with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\model\artifacts\model.pickle", "rb") as f:
-            __model = pickle.load(f)
-
-    print("Artifacts succesfully loaded!")
-
-def get_clean_image(base64_image, file_path):
-    detect_face = cv2.CascadeClassifier(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\opencv\haarcascade_frontalface_default.xml")
-    detect_eye = cv2.CascadeClassifier(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\opencv\haarcascade_eye.xml")
-
-    if file_path:
-        image = cv2.imread(file_path)
-    else:
-        image = base64_to_image(base64_image)
-    
-    if image is not None:
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        face = detect_face.detectMultiScale(gray_image, 1.3, 5)
-
-        cropped_faces = []
-        for (x, y, w, h) in face:
-            region_gray = gray_image[y:y+h, x:x+w]
-            region_colour = image[y:y+h, x:x+w]
-            eyes = detect_eye.detectMultiScale(region_gray)
-            # Check if there are two eyes
-            if len(eyes) >= 2:
-                # Append face if it has two eyes.
-                cropped_faces.append(region_colour)
-    return cropped_faces
-
-
-def base64_to_image(base64_string):
-    encoded_data = base64_string.split(",")[1]
-    nparray = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
-    image = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
-    return image
-
-def get_b64_image():
-    with open(r"C:\Users\ifunanyaScript\Everything\FootballStars_image_classification\server\base64.txt") as f:
-        return f.read()
 
 
 
